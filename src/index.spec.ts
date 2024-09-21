60f4112197f8493fd96e2c5bdce0659b8e461297
Donal Mac An Ri <donal@bluedrop.com>
2024-09-21 19:09:29 -0230

    test: Update test to remove unnecessary `await` keyword

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 describe('translation-tool', () => {
   it('should run without errors', async () => {
-    await expect(import('./index')).resolves.not.toThrow();
+    expect(import('./index')).resolves.not.toThrow();
   });
 });
