bbe9518fa08eb3078f6bb710ed3912d765bbfe80
Donal Mac An Ri <donal@bluedrop.com>
2024-09-21 19:09:29 -0230

    refactor: use node:test instead of jest

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,6 @@
-describe('translation-tool', () => {
-  it('should run without errors', async () => {
-    expect(import('./index')).resolves.not.toThrow();
-  });
+import { test } from 'node:test';
+import assert from 'node:assert';
+
+test('translation-tool', async () => {
+  await assert.doesNotReject(import('./index'));
 });
