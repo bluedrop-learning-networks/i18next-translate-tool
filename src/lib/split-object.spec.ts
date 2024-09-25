d53eec5ae50019102217f96cc12a8373e3d29a8f
Donal Mac An Ri <donal@bluedrop.com>
2024-09-25 08:54:27 -0230

    refactor: Refactor the splitObject function

--- src/lib/split-object.spec.ts
+++ src/lib/split-object.spec.ts
@@ -4,57 +4,57 @@ import splitObject from './split-object';
 import jsonMergePatch from './json-merge-patch';
 
 describe('splitObject', () => {
-  it('should split a large object into smaller ones', () => {
-    const input = {
-      key1: 'value1',
-      key2: {
-        subkey1: 'subvalue1',
-        subkey2: 'subvalue2',
-      },
-      key3: 'value3',
-      key4: {
-        subkey3: {
-          subsubkey1: 'subsubvalue1',
-          subsubkey2: 'subsubvalue2',
-        },
-      },
-    };
+	it('should split a large object into smaller ones', () => {
+		const input = {
+			key1: 'value1',
+			key2: {
+				subkey1: 'subvalue1',
+				subkey2: 'subvalue2',
+			},
+			key3: 'value3',
+			key4: {
+				subkey3: {
+					subsubkey1: 'subsubvalue1',
+					subsubkey2: 'subsubvalue2',
+				},
+			},
+		};
 
-    const result = splitObject(input, 3);
+		const result = splitObject(input, 3);
 
-    assert.ok(result.length > 1, 'Result should have more than one part');
+		assert.ok(result.length > 1, 'Result should have more than one part');
 
-    const reconstructed = result.reduce((acc, patch) => jsonMergePatch(acc, patch), {});
-    assert.deepStrictEqual(reconstructed, input, 'Reconstructed object should match the input');
-  });
+		const reconstructed = result.reduce((acc, patch) => jsonMergePatch(acc, patch), {});
+		assert.deepStrictEqual(reconstructed, input, 'Reconstructed object should match the input');
+	});
 
-  it('should handle nested objects that exceed maxProperties', () => {
-    const input = {
-      key1: {
-        subkey1: 'value1',
-        subkey2: 'value2',
-        subkey3: 'value3',
-        subkey4: 'value4',
-      },
-      key2: {
-        subkey5: 'value5',
-        subkey6: 'value6',
-      },
-    };
+	it('should handle nested objects that exceed maxProperties', () => {
+		const input = {
+			key1: {
+				subkey1: 'value1',
+				subkey2: 'value2',
+				subkey3: 'value3',
+				subkey4: 'value4',
+			},
+			key2: {
+				subkey5: 'value5',
+				subkey6: 'value6',
+			},
+		};
 
-    const result = splitObject(input, 3);
+		const result = splitObject(input, 3);
 
-    assert.ok(result.length > 1, 'Result should have more than one part');
+		assert.ok(result.length > 1, 'Result should have more than one part');
 
-    const reconstructed = result.reduce((acc, patch) => jsonMergePatch(acc, patch), {});
-    assert.deepStrictEqual(reconstructed, input, 'Reconstructed object should match the input');
+		const reconstructed = result.reduce((acc, patch) => jsonMergePatch(acc, patch), {});
+		assert.deepStrictEqual(reconstructed, input, 'Reconstructed object should match the input');
 
-    // Additional assertions
-    assert.strictEqual(result[0].key1.subkey1, 'value1', 'First part should contain key1.subkey1');
-    assert.strictEqual(result[0].key1.subkey2, 'value2', 'First part should contain key1.subkey2');
-    assert.strictEqual(result[0].key1.subkey3, 'value3', 'First part should contain key1.subkey3');
-    assert.strictEqual(result[1].key1.subkey4, 'value4', 'Second part should contain key1.subkey4');
-    assert.strictEqual(result[1].key2.subkey5, 'value5', 'Second part should contain key2.subkey5');
-    assert.strictEqual(result[2].key2.subkey6, 'value6', 'Third part should contain key2.subkey6');
-  });
+		// Additional assertions
+		assert.strictEqual(result[0].key1.subkey1, 'value1', 'First part should contain key1.subkey1');
+		assert.strictEqual(result[0].key1.subkey2, 'value2', 'First part should contain key1.subkey2');
+		assert.strictEqual(result[0].key1.subkey3, 'value3', 'First part should contain key1.subkey3');
+		assert.strictEqual(result[1].key1.subkey4, 'value4', 'Second part should contain key1.subkey4');
+		assert.strictEqual(result[1].key2.subkey5, 'value5', 'Second part should contain key2.subkey5');
+		assert.strictEqual(result[2].key2.subkey6, 'value6', 'Third part should contain key2.subkey6');
+	});
 });
--- src/lib/split-object.ts
+++ src/lib/split-object.ts
@@ -1,55 +1,58 @@
 import { I18nextJson, I18nextJsonMergePatch } from './types';
 
 function countProperties(obj: I18nextJsonMergePatch): number {
-  let count = 0;
-  for (const [, value] of Object.entries(obj)) {
-    count++;
-    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-      count += countProperties(value as I18nextJson);
-    }
-  }
-  return count;
+	let count = 0;
+	for (const [, value] of Object.entries(obj)) {
+		count++;
+		if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+			count += countProperties(value as I18nextJson);
+		}
+	}
+	return count;
 }
 
-export default function splitObject(obj: I18nextJsonMergePatch, maxProperties: number): I18nextJsonMergePatch[] {
-  const result: I18nextJsonMergePatch[] = [];
-  let currentObj: I18nextJsonMergePatch = {};
-  let currentCount = 0;
+export default function splitObject(
+	obj: I18nextJsonMergePatch,
+	maxProperties: number
+): I18nextJsonMergePatch[] {
+	const result: I18nextJsonMergePatch[] = [];
+	let currentObj: I18nextJsonMergePatch = {};
+	let currentCount = 0;
 
-  function addToCurrentObj(path: string[], value: any) {
-    if (currentCount >= maxProperties) {
-      result.push(currentObj);
-      currentObj = {};
-      currentCount = 0;
-    }
+	function addToCurrentObj(path: string[], value: any) {
+		if (currentCount >= maxProperties) {
+			result.push(currentObj);
+			currentObj = {};
+			currentCount = 0;
+		}
 
-    let target = currentObj;
-    for (let i = 0; i < path.length - 1; i++) {
-      if (!(path[i] in target)) {
-        target[path[i]] = {};
-      }
-      target = target[path[i]] as I18nextJsonMergePatch;
-    }
-    target[path[path.length - 1]] = value;
-    currentCount++;
-  }
+		let target = currentObj;
+		for (let i = 0; i < path.length - 1; i++) {
+			if (!(path[i] in target)) {
+				target[path[i]] = {};
+			}
+			target = target[path[i]] as I18nextJsonMergePatch;
+		}
+		target[path[path.length - 1]] = value;
+		currentCount++;
+	}
 
-  function processObject(o: I18nextJson, path: string[] = []) {
-    for (const [key, value] of Object.entries(o)) {
-      const newPath = [...path, key];
-      if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
-        processObject(value as I18nextJson, newPath);
-      } else {
-        addToCurrentObj(newPath, value);
-      }
-    }
-  }
+	function processObject(o: I18nextJson, path: string[] = []) {
+		for (const [key, value] of Object.entries(o)) {
+			const newPath = [...path, key];
+			if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+				processObject(value as I18nextJson, newPath);
+			} else {
+				addToCurrentObj(newPath, value);
+			}
+		}
+	}
 
-  processObject(obj);
+	processObject(obj);
 
-  if (Object.keys(currentObj).length > 0) {
-    result.push(currentObj);
-  }
+	if (Object.keys(currentObj).length > 0) {
+		result.push(currentObj);
+	}
 
-  return result;
+	return result;
 }
